8d6fe9bc1c41c0819716523004896f28f10f63d0
toastylol <leonardjohn974@gmail.com>
2026-02-25 04:18:16 +0530

    added pm2 ecosystem config file

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [{
+    name: "ineffa",
+    script: "./gemineffa.js",
+    watch: false, 
+    
+    autorestart: true,
+    max_restarts: 10,
+    restart_delay: 4000, 
+    
+    max_memory_restart: "200M", 
+    
+    out_file: "./logs/out.log",
+    error_file: "./logs/error.log",
+    merge_logs: true,
+    log_date_format: "DD-MM-YYYY HH:mm:ss",
+
+    env: {
+      NODE_ENV: "production",
+    }
+  }]
+}
