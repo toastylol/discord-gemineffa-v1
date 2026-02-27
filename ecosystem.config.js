90d3153378de2678303c7256fd64f094fe675c3f
toastylol <leonardjohn974@gmail.com>
2026-02-27 17:15:11 +0530

    added graceful shutdowns

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,16 @@ module.exports = {
   apps: [{
     name: "ineffa",
     script: "./gemineffa.js",
-    watch: false, 
+    watch: false,
+    kill_timeout: 3000,
+    wait_ready: true,
+    listen_timeout: 3000,
     
     autorestart: true,
     max_restarts: 10,
-    restart_delay: 4000, 
+    restart_delay: 4000,
     
-    max_memory_restart: "200M", 
+    max_memory_restart: "200M",
     
     out_file: "./logs/out.log",
     error_file: "./logs/error.log",
